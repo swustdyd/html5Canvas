552a92ba2f5c42fdfce2ac5296a8207826213d1c
aarondeng <1562044678@qq.com>
2017-04-24 17:32:51 +0800

    add waterMark

--- jQuery/jquery-canvas-images.js
+++ jQuery/jquery-canvas-images.js
@@ -3,15 +3,31 @@
  */
 ;(function ($) {
     var canvas,
-        context;
+        context,
+        waterMarkCanvas,
+        waterMarkContext,
+        addWaterMark = true,
+        waterContent = "dyd";
+
     $.fn.extend({
             "drawMyImage": function (url, scale) {
                 canvas = document.getElementById(this.attr("id"));
+                waterMarkCanvas = document.getElementById("waterMarkCanvas");
+                waterMarkCanvas.width = canvas.width * 0.2;
+                waterMarkCanvas.height = canvas.height * 0.1;
                 try {
                     context = canvas.getContext("2d");
+                    waterMarkContext = waterMarkCanvas.getContext("2d");
                 } catch (e) {
                     throw "function 'drawMyImage' only supported by canvas tag and must be have id for attribute";
                     return;
+                }
+                if(addWaterMark){
+                    waterMarkContext.strokeStyle = "rgba(255,255,255,0.5)";
+                    waterMarkContext.baseline = "middle";
+                    waterMarkContext.textAlign = "right";
+                    waterMarkContext.font = "24px sans-serif italic";
+                    waterMarkContext.fillText(waterContent, waterMarkCanvas.width/2, waterMarkCanvas.height/2);
                 }
                 var image = new Image();
                 image.src = url;
@@ -23,6 +39,7 @@
                     var dy = (canvas.height - drawHeight) / 2;
                     context.clearRect(0, 0, canvas.width, canvas.height);
                     context.drawImage(image, dx, dy, drawWidth, drawHeight);
+                    context.drawImage(waterMarkCanvas, canvas.width/2, canvas.height - waterMarkCanvas.height);
                     /*context.drawImage(image, 20, 20, image.width - 20, image.height - 20,
                         image.width, image.height, image.width - 20, image.height - 20);*/
                 }
